970a01708af731ee55a637563d31df5f0e09558d
Ery Lee <ery.lee@gmail.com>
2011-10-19 17:40:27 +0800

    update sched.c

--- src/sched.c
+++ src/sched.c
@@ -9,9 +9,12 @@
 #include "smarta.h"
 #include "common.h"
 
+#define MAX_INPUT_BUFFER 4096
+
 extern smarta_t smarta;
 
 static int check_service(xmpp_conn_t * const conn, void * const userdata);
+static void send_message(xmpp_conn_t * conn, sds result);
 
 void sched_services(xmpp_ctx_t *ctx, xmpp_conn_t *conn) {
     service_t *service;
@@ -21,14 +24,47 @@ void sched_services(xmpp_ctx_t *ctx, xmpp_conn_t *conn) {
         service = (service_t *)node->value;
         printf("sched service: %s\n", service->name);
         //FUCK: could only add one handler!!!
-        handler_add_timed(conn, check_service, service->period*1000, service);
+        handler_add_timed(conn, check_service, service->period*60*1000, service);
     }
 }
 
 int check_service(xmpp_conn_t *conn, void *userdata) {
+    FILE *fp = NULL;
+	char output_buffer[MAX_INPUT_BUFFER] = "";
+	sds raw_command = sdsnew("cd /opt/csmarta/plugins ; ./");
+    sds result =sdsempty();
     service_t *service = (service_t *)userdata;
     printf("check service: %s\n", service->name);
+    raw_command = sdscat(raw_command, service->command);
+    fp = popen(raw_command, "r");
+    while(fgets(output_buffer, sizeof(output_buffer) - 1, fp)) {
+        result = sdscat(result, output_buffer);
+    }
+    pclose(fp);
+    printf("check result:  %s\n", result);
+
+    send_message(conn, result);
+    sdsfree(result);
     return 1;
 }
 
-
+void send_message(xmpp_conn_t *conn, sds result) {
+    xmpp_stanza_t *reply, *body, *text;
+    
+	reply = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_name(reply, "message");
+	xmpp_stanza_set_type(reply, "chat");
+	xmpp_stanza_set_attribute(reply, "to", "erylee@nodehub.cn");
+	
+	body = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_name(body, "body");
+	
+	text = xmpp_stanza_new(ctx);
+	xmpp_stanza_set_text(text, result);
+	xmpp_stanza_add_child(body, text);
+	xmpp_stanza_add_child(reply, body);
+	
+	xmpp_send(conn, reply);
+	xmpp_stanza_release(reply);
+}
+    
